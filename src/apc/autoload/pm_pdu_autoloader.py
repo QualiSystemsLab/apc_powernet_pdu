384199e5a4712a3810239b9db5c12f9d3b821d76
ksaper <kimo.s@qualisystems.com>
2017-09-25 11:39:55 -0700

    Updated Gen2 based Gen1 Testing
    Updated MIB Table entries based on Gen1 Testing

--- src/apc/autoload/pm_pdu_autoloader.py
+++ src/apc/autoload/pm_pdu_autoloader.py
@@ -14,22 +14,26 @@ class PmPduAutoloader:
         rv.resources = []
         rv.attributes = []
 
-        rv.attributes.append(self.makeattr('', 'Location', self.snmp_handler.get_property('SNMPv2-MIB', 'sysLocation', 0)))
-        rv.attributes.append(self.makeattr('', 'Model', self.snmp_handler.get_property('PM-MIB', 'pmProductModel', 0)))
-        rv.attributes.append(self.makeattr('', 'Serial Number', self.snmp_handler.get_property('PM-MIB', 'pmSerialNumber', 0)))
-        rv.attributes.append(self.makeattr('', 'Vendor', 'Avocent'))
-        rv.attributes.append(self.makeattr('', 'Version', self.snmp_handler.get_property('PM-MIB', 'pmFirmwareVersion', 0)))
+        # rv.attributes.append(self.makeattr('', 'Location', self.snmp_handler.get_property('SNMPv2-MIB', 'sysLocation', 0)))
+        # this should be correct - issues in the Gen1Shell & the modeling did allow for testing:
+        rv.attributes.append(self.makeattr('', 'Location', self.snmp_handler.get_property('PowerNet-MIB', 'rPDU2IdeLocationnt', 0)))
+        rv.attributes.append(self.makeattr('', 'Model', self.snmp_handler.get_property('PowerNet-MIB', 'sPDUIdentModelNumber', 0)))
+        # optional (Serial Number not part of the standard):
+        # rv.attributes.append(self.makeattr('', 'Serial Number', self.snmp_handler.get_property('PowerNet-MIB', 'sPDUIdentSerialNumber', 0)))
+        rv.attributes.append(self.makeattr('', 'Vendor', 'APC'))
+        rv.attributes.append(self.makeattr('', 'Firmware Version', self.snmp_handler.get_property('PowerNet-MIB', 'sPDUIdentFirmwareRev', 0)))
 
-        pdu_name = self.snmp_handler.get_property('PM-MIB', 'pmHostName', 0)
+        pdu_name = self.snmp_handler.get_property('PowerNet-MIB', 'sPDUMasterConfigPDUName', 0)
 
-        outlet_table = self.snmp_handler.get_table('PM-MIB', 'pmPowerMgmtOutletsTable')
+        outlet_table = self.snmp_handler.get_table('PowerNet-MIB', 'sPDUOutletControlTable')
         for index, attribute in outlet_table.iteritems():
             name = 'Outlet %s' % index
             relative_address = index
             unique_identifier = '%s.%s' % (pdu_name, index)
 
             rv.resources.append(self.makeres(name, 'Generic Power Socket', relative_address, unique_identifier))
-            rv.attributes.append(self.makeattr(relative_address, 'Port Description', attribute['pmPowerMgmtOutletsTablePortName']))
+            # need to check this line too:
+            rv.attributes.append(self.makeattr(relative_address, 'Port Description', attribute['memOutletStatusOutletName']))
 
         return rv
 
